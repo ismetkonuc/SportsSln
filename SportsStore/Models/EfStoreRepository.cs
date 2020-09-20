888835f5744acc1a2cbb680c89d661a781833a8a
İsmet KONUÇ <17155035@mersin.edu.tr>
2020-09-20 04:36:05 +0300

    Add project files.

---
+++ SportsStore/Models/EfStoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EfStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EfStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
